260230f676b86d701c788f2119d107747912a04f
rhgcastel <rodrigo.castel@wed2b.co.uk>
2023-05-09 09:14:05 +0100

    Added image-pro files

---
+++ image_pro/backend/app.py
@@ -0,0 +1,94 @@
+from flask import Flask, request, send_from_directory, jsonify, url_for
+from flask_cors import CORS
+from PIL import Image, ImageOps
+import piexif
+import os
+
+app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:3000"])
+
+input_dir = "./input"
+output_dir = "./output"
+
+if not os.path.exists(input_dir):
+    os.makedirs(input_dir)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_files = request.files.getlist('file[]')  # Get all files as a list
+    width = request.form.get('width')
+    height = request.form.get('height')
+    optimize = request.form.get('optimize') == 'True'
+    quality = int(request.form.get('quality')) if optimize else None
+
+    response_data = []
+
+    for uploaded_file in uploaded_files:
+        print(f"Processing file: {uploaded_file.filename}")
+        filename = uploaded_file.filename
+
+        if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
+            input_path = os.path.join(input_dir, filename)
+            uploaded_file.save(input_path)
+
+            img = Image.open(input_path)
+
+            original_size = os.path.getsize(input_path)
+
+            if optimize:
+                # Remove EXIF data
+                try:
+                    exif_dict = piexif.load(img.info["exif"])
+                    exif_bytes = piexif.dump(exif_dict)
+                except (AttributeError, KeyError):
+                    exif_bytes = b''
+
+                img_optimized = ImageOps.autocontrast(img, cutoff=0, ignore=None)
+                output_path = os.path.join(output_dir, filename)
+                img_optimized.save(output_path, optimize=True, quality=quality, exif=exif_bytes)
+            else:
+                if width and height:
+                    img_resized = img.resize((int(width), int(height)), resample=Image.LANCZOS)
+                else:
+                    img_resized = img
+
+                # Convert image to RGB mode if it has an alpha channel (transparency)
+                if img_resized.mode == 'RGBA':
+                    img_resized = img_resized.convert('RGB')
+
+                output_path = os.path.join(output_dir, filename)
+                img_resized.save(output_path)
+
+            optimized_size = os.path.getsize(output_path)
+            optimized_image_url = url_for('get_image', filename=filename, _external=True)
+            original_width, original_height = img.size
+            new_width, new_height = img_resized.size
+            print("Optimized image URL:", optimized_image_url)
+
+            response_data.append({
+                "filename": filename,
+                "original_size": original_size,
+                "optimized_size": optimized_size,
+                "original_width": original_width,
+                "original_height": original_height,
+                "new_width": new_width,
+                "new_height": new_height,
+                "optimized_image_url": optimized_image_url,
+                "optimized_size_bytes": optimized_size,
+            })
+        else:
+            return "Invalid file format", 400
+
+    return jsonify(response_data)
+
+@app.route('/output/<filename>')
+def get_image(filename):
+    return send_from_directory(output_dir, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ image_pro/frontend/src/components/ImageItem.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Box, CardMedia, Button, Slider } from '@mui/material';
+import useDebounce from '../hooks/useDebounce';
+
+const ImageItem = ({image, setImages}) => {
+    const [inputQuality, setInputQuality] = useState(image.quality);
+    const debouncedQuality = useDebounce(inputQuality, 300);
+    const isOptimized = image.operation === "Optimized";
+
+    useEffect(() => {
+        updateQuality(debouncedQuality);
+    }, [debouncedQuality]);
+
+    const downloadImage = () => {
+        fetch(image.optimizedImage)
+            .then((response) => response.blob())
+            .then((blob) => {
+                const link = document.createElement('a');
+                link.href = URL.createObjectURL(blob);
+                link.download = isOptimized ? 'optimized_image.jpg' : 'resized_image.jpg';
+                link.style.display = 'none';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            });
+    };
+
+    const dataUrlToImage = (dataUrl) => {
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.src = dataUrl;
+            img.onload = () => resolve(img);
+        });
+    };
+
+    const optimizeImage = async (img, quality) => {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        return await new Promise((resolve) => {
+            canvas.toBlob(
+                (blob) => {
+                    const data = URL.createObjectURL(blob);
+                    resolve({ canvas, data });
+                },
+                "image/jpeg",
+                quality / 100
+            );
+        });
+    };
+
+    const updateQuality = async (newQuality) => {
+        const img = await dataUrlToImage(image.originalImage);
+        const { canvas, data } = await optimizeImage(img, newQuality);
+        const response = await fetch(data);
+        const blob = await response.blob();
+        const updatedOptimizedSize = blob.size;
+        const updatedImage = {
+            ...image,
+            optimizedImage: data,
+            optimizedSizeMB: (updatedOptimizedSize / (1024 * 1024)).toFixed(2),
+            quality: newQuality,
+        };
+        setImages((prevImages) =>
+            prevImages.map((prevImage) => (prevImage === image ? updatedImage : prevImage))
+        );
+    };
+
+
+
+    return (
+        <Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div" align='center'>
+                    {isOptimized ? 'Optimized Image' : 'Resized Image'}
+                </Typography>
+                <Box sx={{display: 'flex', justifyContent: 'space-between', mt: 2}}>
+                    <CardMedia
+                        component="img"
+                        alt="Original Image"
+                        image={image.originalImage}
+                        title="Original Image"
+                        sx={{maxWidth: '200px', ml: -2, mr: 2}}
+                    />
+                    <CardMedia
+                        component="img"
+                        alt={isOptimized ? "Optimized Image" : "Resized Image"}
+                        image={image.optimizedImage}
+                        title={isOptimized ? "Optimized Image" : "Resized Image"}
+                        sx={{maxWidth: '200px'}}
+                    />
+                </Box>
+                <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                    {isOptimized ? (
+                        <Box sx={{justifyContent: 'space-between' }}>
+                            <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
+                                <Typography variant="body2" color="text.secondary" align="center">
+                                    Original size: {image.originalSizeMB} MB
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" align="center">
+                                    Optimized size: {image.optimizedSizeMB} MB
+                                </Typography>
+                            </Box>
+                            <Typography mt={1} variant="body2" color="text.secondary" align="center" gutterBottom>Quality ({image.quality})</Typography>
+                            <Slider
+                                value={inputQuality}
+                                onChange={(event, newValue) => setInputQuality(newValue)}
+                                valueLabelDisplay="auto"
+                                step={1}
+                                marks
+                                min={1}
+                                max={100}
+                                sx={{ marginBottom: 2 }}
+                            />
+                        </Box>
+                    ) : (
+                        <>
+                            <Typography variant="body2" color="text.secondary" align="center">
+                                Original dimensions: {image.originalImageWidth}x{image.originalImageHeight}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" align="center">
+                                Resized dimensions: {image.newImageWidth}x{image.newImageHeight}
+                            </Typography>
+                        </>
+                    )}
+                </Box>
+
+
+                <Box sx={{display: 'flex', justifyContent: 'space-around'}} mt={2}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={downloadImage}
+                    >
+                        Download
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => window.open(image.optimizedImage, '_blank')}
+                    >
+                        Open
+                    </Button>
+                </Box>
+
+
+            </CardContent>
+        </Card>
+    );
+};
+
+export default ImageItem;
